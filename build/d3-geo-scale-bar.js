e1867d0309b6735fc3021353c8b8f6cf13a09d6b
HarryStevens <harryjosephstevens@gmail.com>
2019-12-30 09:42:39 -0500

    0.3.1

--- build/d3-geo-scale-bar.js
+++ build/d3-geo-scale-bar.js
@@ -1,4 +1,4 @@
-// https://github.com/HarryStevens/d3-geo-scale-bar Version 0.3.0. Copyright 2019 Harry Stevens.
+// https://github.com/HarryStevens/d3-geo-scale-bar Version 0.3.1. Copyright 2019 Harry Stevens.
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
   typeof define === 'function' && define.amd ? define(['exports'], factory) :
