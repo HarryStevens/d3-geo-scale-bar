1261ec1ff3c2a2843b56abd3cb2f10a2c766573d
HarryStevens <harryjosephstevens@gmail.com>
2020-02-11 18:19:09 -0500

    Remove unused dependency

--- build/d3-geo-scale-bar.js
+++ build/d3-geo-scale-bar.js
@@ -260,7 +260,7 @@
       if (distance) {
         barDistance = distance;
         barWidth = barDistance / (geoDistance(start, projection.invert([x + 1, y])) * radius);
-      } // Otherwise, make it an exponent of 10 with a minimum width of 40px 
+      } // Otherwise, make it an exponent of 10 or 10x4 with a minimum width of 60px 
       else {
           var dist = .01,
               minWidth = 60,
